1e2985ce5271ede7dd057879c92ca7e811ada597
huu.pham <huu.pham@arenathemes.com>
2020-09-12 12:10:40 +0700

    update code

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -1,5 +1,6 @@
 const Router = require('koa-router');
 const route = new Router();
+var bodyParser = require('koa-bodyparser');
 
 const WidgetsMiddlewares = require('./../middlewares/widgets');
 const TemplatesMiddlewares = require('./../middlewares/templates');
@@ -9,8 +10,10 @@ route.get('/widgets', async (ctx) => {
     ctx.body = res;
 });
 
-route.post('/widgets', async (ctx) => {
-    const res = await WidgetsMiddlewares.update();
+route.post('/widgets', bodyParser(), async (ctx) => {
+    const { data_stringfy } = ctx.request.body;
+    // console.log('data_stringfy :>> ', data_stringfy);
+    const res = await WidgetsMiddlewares.update(data_stringfy);
     ctx.body = res;
 });
 
@@ -20,7 +23,9 @@ route.get('/templates', async (ctx) => {
 });
 
 route.post('/templates', async (ctx) => {
-    const res = await TemplatesMiddlewares.update();
+    const { data_stringfy } = ctx.request.body;
+    // console.log('data_stringfy :>> ', data_stringfy);
+    const res = await TemplatesMiddlewares.update(data_stringfy);
     ctx.body = res;
 });
 
